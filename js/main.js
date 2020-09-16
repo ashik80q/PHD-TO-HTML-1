864d836728119247db87a744143ccadb9933d5c2
Ashik khandakar <ashikbpdb@gmail.com>
2020-09-16 18:20:08 +0600

    code writing complete and added responsive

--- js/main.js
+++ js/main.js
@@ -2,4 +2,8 @@
 
 $(document).ready(function(){
     $('.slider_area').bxSlider();
+    $('#main_menu').meanmenu({
+      meanMenuContainer: '#mobile_menu',
+      meanScreenWidth: 991,
+    });
   });
